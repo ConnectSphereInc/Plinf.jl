51bd1109e0d80922c8ef58e3a67604894d223c8b
Nanami <88866882+krmr73@users.noreply.github.com>
2024-06-16 21:17:19 +0900

    update: change to use random_planner

--- examples/vision/example.jl
+++ examples/vision/example.jl
@@ -4,6 +4,7 @@ using Gen, GenParticleFilters
 using PDDLViz, GLMakie
 
 include("utils.jl")
+include("random_planner.jl")
 
 # Register PDDL array theory
 println("Registering PDDL array theory")
@@ -13,7 +14,7 @@ PDDL.Arrays.register!()
 println("Loading domain")
 domain = load_domain(joinpath(@__DIR__, "domain.pddl"))
 println("Loading problem")
-problem = load_problem(joinpath(@__DIR__, "problems", "problem-2.pddl"))
+problem = load_problem(joinpath(@__DIR__, "problems", "problem-1.pddl"))
 
 # Initialize state and construct goal specification
 println("Initializing state")
@@ -29,7 +30,7 @@ domain, state = PDDL.compiled(domain, state)
 #--- Define Renderer ---#
 println("Defining renderer")
 # Construct gridworld renderer
-gem_colors = PDDLViz.colorschemes[:vibrant]
+
 renderer = PDDLViz.GridworldRenderer(
     resolution = (600, 700),
     agent_renderer = (d, s) -> begin
@@ -66,8 +67,9 @@ save("examples/vision/initial_state.png", canvas)
 #--- Visualize Plans ---#
 
 # Check that A* heuristic search correctly solves the problem
-planner = AStarPlanner(GoalManhattan(), save_search=true)
-sol = planner(domain, state, spec)
+random_planner = RandomPlanner(save_search=true)
+# astar_planner = AStarPlanner(GoalManhattan(), save_search=true)
+sol = random_planner(domain, state, spec)
 
 # Visualize resulting plan
 plan = collect(sol)
