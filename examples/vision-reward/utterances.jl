ff34477df68f4d3319494456c2f877db23b75158
ciaran-regan-ie <ciaranregan123@gmail.com>
2024-08-21 12:37:21 +0900

    working particle filter

--- examples/vision-reward/utterances.jl
+++ examples/vision-reward/utterances.jl
@@ -2,7 +2,7 @@ using Gen, GenGPT3
 using Random
 using DotEnv
 import Gen: ParticleFilterState
-import GenParticleFilters: pf_initialize, pf_update!, pf_resample!, pf_rejuvenate!, pf_move_accept!, get_norm_weights, get_traces, effective_sample_size
+import GenParticleFilters: pf_initialize, pf_update!, pf_resample!, pf_rejuvenate!, get_norm_weights, get_traces, effective_sample_size
 
 overlay = DotEnv.config()
 api_key = get(overlay, "OPENAI_API_KEY", nothing)
@@ -12,8 +12,8 @@ ENV["OPENAI_API_KEY"] = api_key
 
 gpt3 = GPT3GF(model="davinci-002", stop="\n", max_tokens=512, temperature=1)
 
-global gems = ["red", "blue", "yellow", "green"]
-global g_rewards::Dict{String, Int} = Dict("red" => 5, "blue" => 3, "yellow" => 1, "green" => -1)
+global possible_gems = ["red", "blue", "yellow", "green"]
+global possible_rewards = [-1, 1, 3, 5]
 
 global EXAMPLES_PICKUP = [
     ("Gem: red\nReward: +5", "I picked up a red gem and got +5 reward!"),
@@ -48,6 +48,11 @@ global EXAMPLES_NO_PICKUP = [
 shuffle!(EXAMPLES_PICKUP)
 shuffle!(EXAMPLES_NO_PICKUP)
 
+"""
+    construct_prompt(context::String, examples::Vector{Tuple{String, String}})
+
+    Construct a prompt for the GPT-3 model based on a context and examples.
+"""
 function construct_prompt(context::String, examples::Vector{Tuple{String, String}})
     example_strs = ["$ctx\nUtterance: $utt" for (ctx, utt) in examples]
     example_str = join(example_strs, "\n")
@@ -55,31 +60,59 @@ function construct_prompt(context::String, examples::Vector{Tuple{String, String
     return prompt
 end
 
+"""
+    get_top_weighted_rewards(state::ParticleFilterState, n::Int)
+
+    Get the top `n` most likely reward distributions based on the particle filter state.
+
+"""
 function get_top_weighted_rewards(state::ParticleFilterState, n::Int)
     traces = get_traces(state)
     weights = get_norm_weights(state)
     reward_weights = Dict()
+    
+    # Accumulate rewards and weights
     for (tr, w) in zip(traces, weights)
-        rewards = Dict{String, Int}()
-        for gem in gems
-            rewards[gem] = tr[:reward => Symbol(gem)]
+        rewards = Dict{String, String}()
+        for gem in possible_gems
+            rewards[gem] = string(tr[:reward => Symbol(gem)])
         end
         rewards_tuple = Tuple(sort(collect(rewards)))
         reward_weights[rewards_tuple] = get(reward_weights, rewards_tuple, 0.0) + w
     end
-    weighted_rewards = [(Dict(zip(gems, k)), v) for (k, v) in reward_weights]
+    
+    total_weight = sum(values(reward_weights))
+    weighted_rewards = [(Dict(rewards), weight / total_weight) 
+                        for (rewards, weight) in reward_weights]
+    
+    # Sort by probability
     sort!(weighted_rewards, by = x -> x[2], rev = true)
-    return values(weighted_rewards[1:min(n, length(weighted_rewards))])
+    
+    # Return top n results
+    return weighted_rewards[1:min(n, length(weighted_rewards))]
 end
 
+"""
+    utterance_model(T::Int)
 
+    Generate a sequence of utterances based on simulated gem pickups and rewards.
+
+    First, a reward is assigned to each gem. Then, for each timestep `t` in `1:T`,
+    an agent either picks up a gem or not. If a gem is picked up, the agent recieves
+    an award corresponding to the gem. The agent then utters a sentence based on the
+    gem picked up and the reward received. If no gem is picked up, the agent utters
+    a sentence based on the fact that no gem was picked up.
+
+    At inference time, the agent observes a sequence of utterances and infers the
+    rewards for each gem.
+"""
 @gen function utterance_model(T::Int)
-    global EXAMPLES_PICKUP, EXAMPLES_NO_PICKUP, gems, g_rewards    
+    global EXAMPLES_PICKUP, EXAMPLES_NO_PICKUP, possible_gems, possible_rewards    
     
     # What are the rewards for each gem?
     rewards::Dict{String, Int} = Dict()
-    for gem in gems
-        rewards[gem] = {:reward => Symbol(gem)} ~ labeled_uniform(collect(values(g_rewards)))
+    for gem in possible_gems
+        rewards[gem] = {:reward => Symbol(gem)} ~ labeled_uniform(possible_rewards)
     end
 
     utterances = []
@@ -88,8 +121,7 @@ end
         gem_pickup = {t => :gem_pickup} ~ bernoulli(0.5)
         if gem_pickup
             # Which gem?
-            gem = {t => :gem} ~ labeled_uniform(gems)
-            # reward = {t => :reward => Symbol(gem)} ~ labeled_uniform(collect([rewards[gem]]))
+            gem = {t => :gem} ~ labeled_uniform(possible_gems)
             reward = rewards[gem]
             context = "Gem: $gem\nReward: $reward"
             prompt = construct_prompt(context, EXAMPLES_PICKUP)
@@ -105,79 +137,68 @@ end
     return utterances
 end
 
+"""
+    gem_from_utterance(utterance::String)
+
+    Manually extract the color of a gem from an utterance using regex.
+"""
 function gem_from_utterance(utterance::String)
-    probs = Dict{String, Float64}()
-    constraints = Gen.choicemap()
-    constraints[(1 => :utterance => :output)] = utterance
-    constraints[(1 => :gem_pickup)] = true
-    for gem in gems
-        constraints[(1 => :gem)] = gem
-        _, w = Gen.generate(utterance_model, (1,), constraints)
-        probs[gem] = exp(w)
+    color_pattern = r"\b(red|blue|yellow|green)\b"
+    match_result = match(color_pattern, lowercase(utterance))
+    
+    if match_result !== nothing
+        return String(match_result.match)
     end
-    most_likely = argmax(probs)
-    return most_likely
+
+    return nothing
 end
 
-function reward_from_utterance(utterance::String, gem::String)
-    global rewards
-    probs = Dict{Int, Float64}()
-    constraints = Gen.choicemap()
-    constraints[(1 => :utterance => :output)] = utterance
-    constraints[(1 => :gem_pickup)] = true
-    constraints[(1 => :gem)] = gem
-    for reward in values(g_rewards)
-        constraints[(:reward => Symbol(gem))] = reward
-        _, w = Gen.generate(utterance_model, (1,), constraints)
-        probs[reward] = exp(w)
+"""
+    reward_from_utterance(utterance::String, gem::String)
+
+    Manually extract the reward from an utterance using regex.
+"""
+function reward_from_utterance(utterance::String)
+    score_pattern = r"\b(-1|1|3|5)\b"
+    match_result = match(score_pattern, utterance)
+    
+    if match_result !== nothing
+        return parse(Int, match_result.match)
     end
-    most_likely = argmax(probs)
-    return most_likely
+
+    return nothing
 end
 
-# function gem_from_utterance(utterance::String)
-#     color_pattern = r"\b(red|blue|yellow|green)\b"
-#     match_result = match(color_pattern, lowercase(utterance))
+"""
+    particle_filter(utterances, n_particles; ess_thresh=0.5, infer_gem=false)
+
+    Perform particle filtering to infer the rewards for each gem based on a sequence of utterances.
     
-#     if match_result !== nothing
-#         return String(match_result.match)
-#     end
-
-#     return nothing
-# end
-
-# function reward_from_utterance(utterance::String, gem::String)
-#     score_pattern = r"\b(-1|1|3|5)\b"
-#     match_result = match(score_pattern, utterance)
-    
-#     if match_result !== nothing
-#         return parse(Int, match_result.match)
-#     end
-
-#     return nothing
-# end
-
-function particle_filter(utterances, n_particles, ess_thresh=0.5)
+    The agent observed a series of gem pickups and utterances and infers the rewards for each type
+    of gem. The agent can either infer the gem type from the utterance or observe the gem type directly.
+"""
+function particle_filter(utterances, n_particles; ess_thresh=0.5, infer_gem=false)
     n_obs = length(utterances)
     observations = []
 
     for (i, utterance) in enumerate(utterances)
-        gem = gem_from_utterance(utterance)
-        reward = reward_from_utterance(utterance, gem)
-
-        println("gem: $gem, reward: $reward")
 
         observation = Gen.choicemap()
         observation[i => :utterance => :output] = utterance
         observation[i => :gem_pickup] = true
-        observation[i => :gem] = gem
+
+        if !infer_gem
+            gem = gem_from_utterance(utterance)
+            observation[i => :gem] = gem
+        end
+
         push!(observations, observation)
     end
 
     state = pf_initialize(utterance_model, (1,), observations[1], n_particles)
     for t=2:n_obs
         if effective_sample_size(state) < ess_thresh * n_particles
-            pf_resample!(state, :residual)
+            pf_resample!(state, :stratified)
             rejuv_sel = select()
             pf_rejuvenate!(state, mh, (rejuv_sel,))
         end
@@ -192,10 +213,7 @@ utterances = [
     # "Picked up a red gem, +5!",
 ]
 
-state = particle_filter(utterances, 20)
-
-# trace = get_traces(state)[1]
-# println(display(Gen.get_choices(trace)))
+state = particle_filter(utterances, 100, infer_gem=false)
 
 top_rewards = get_top_weighted_rewards(state, 10)
 println("Top 5 most likely reward estimates:")
